cfee134171d405c6f46ab4d7960be48f4133a0d7
Mr. Young <masyoung@hawaii.edu>
2020-07-24 00:01:53 -1000

    almost finished 15.1. Just need to clean up the code

--- Event-Driven_and_Animation/Exercise15_01.java
+++ Event-Driven_and_Animation/Exercise15_01.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.BoarderPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -13,46 +15,72 @@ import java.util.ArrayList;
 
 /** Lets the user click the Refresh botton to display four cards */
 public class Exercise15_01 extends Application {
+	private Deal hand = new Deal();
+
 	@Override
 	public void start(Stage primaryStage) {
-		// Create four random card image objects
-		HBox hbox = new HBox();
-		hbox.spacing(5);
-		hbox.alignment(Pos.CENTER);
-
-		int[] numbers = randomNumbers();
-		Image[] cards = new Image[4];
-		for (int i = 0; i < 4; i++) {
-			cards[i] = new Image("card/"numbers[i + 1]".png");
-			hbox.getChildren().add(new ImageView(cards[i]));
-		}
-
 		// Place Refresh button into a stackpane
+		HBox hbox = new HBox();
+		hbox.setPadding(new Insets(5, 10, 10, 10));
+		hbox.setAlignment(Pos.CENTER);
+		Button btRefresh = new Button("Refresh");
+		hbox.getChildren().add(btRefresh);
 
 		// Create and register the handler for the button
+		btRefresh.setOnAction(e -> hand.updateCards());
 
-		// Place hbox and stackpane into a gridpane
+		// Place hbox and stackpane into a borderpane
+		BorderPane pane = new BorderPane();
+		pane.setCenter(hand);
+		pane.setBottom(hbox);
+		pane.setAlignment(hbox, Pos.CENTER);
 
 		// Create scene and place into the stage
-		Scene scene = new Scene(pane, 400, 300);
+		Scene scene = new Scene(pane);
 		primaryStage.setTitle("Cards");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
-	private int[] randomNumbers() {
-		ArrayList<Integer> cardNames = new ArrayList<>(52);
-		for (int num = 1; num <= 52; num++) {
-			cardNames.add(num);
+	
+	public class Deal extends HBox {
+		private ImageView[] cards = new ImageView[4];
+
+		public Deal() {
+			setAlignment(Pos.CENTER);
+			setSpacing(3);
+			setPadding(new Insets(10, 10, 5, 10));
+			initializeCards();
 		}
 
-		Random r = new Random();
-		int[] numbers = new int[4];
-		for (int i = 0; i < 4; i++) {
-			numbers[i] = cardNames.remove(r.nextInt(51 - i));
+		private void initializeCards() {
+			int[] numbers = randomNumbers();
+			for (int i = 0; i < 4; i++) {
+				cards[i] = new ImageView("card/" + numbers[i] + ".png");
+				getChildren().add(cards[i]);
+			}
 		}
 
-		return numbers;
+		public void updateCards() {
+			int[] numbers = randomNumbers();
+			for (int i = 0; i < 4; i++) {
+				cards[i].setImage(new Image("card/" + numbers[i] + ".png"));
+			}
+		}
+
+		private int[] randomNumbers() {
+			ArrayList<Integer> cardNames = new ArrayList<>(52);
+			for (int num = 1; num <= 52; num++) {
+				cardNames.add(num);
+			}
+
+			Random r = new Random();
+			int[] numbers = new int[4];
+			for (int i = 0; i < 4; i++) {
+				numbers[i] = cardNames.remove(r.nextInt(51 - i));
+			}
+			return numbers;
+		}
 	}
 
 	public static void main(String[] args) {
